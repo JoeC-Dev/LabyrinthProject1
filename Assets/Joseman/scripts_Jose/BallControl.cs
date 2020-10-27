6a642625654afe26de6be52977a5cba104ea399a
SevenPebbles <54554298+SevenPebbles@users.noreply.github.com>
2020-10-27 15:09:56 -0400

    Ball movement script

---
+++ Assets/Joseman/scripts_Jose/BallControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(hInput, 0.0f, vInput) * moveSpeed * Time.deltaTime);
+    }
+}
